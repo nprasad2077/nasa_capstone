083088e8d58813ed369f0c968f952a2e635a649b
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-30 13:22:56 -0500

    axios fetch and render data asod

--- src/components/ASOD.jsx
+++ src/components/ASOD.jsx
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
-
 const ASOD = () => {
+  const [asod, setAsod] = useState([]);
+
+  const getAsod = async () => {
+    const response = await axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${
+          import.meta.env.VITE_APP_NASA_API_KEY
+        }`
+      )
+      .then((res) => {
+        setAsod(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getAsod();
+  }, []);
+
+  console.log(asod);
+
   return (
-    <div className="card w-96 bg-base-100 text-neutral-content " >
+    <div className="card w-96 bg-base-100 text-neutral-content ">
       <figure className="px-10 pt-10">
         <img
-          src="https://apod.nasa.gov/apod/image/2305/M27_Cosgrove_2717.jpg"
+          src={asod.hdurl}
           alt="ASOD"
           className="rounded-xl"
         />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{asod.title}</h2>
+        <p>{asod.explanation}</p>
       </div>
     </div>
   );
