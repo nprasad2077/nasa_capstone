456312f9bae9693f17e6587f524f82ce0d2feecd
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-31 23:16:06 -0500

    asteroid watch card

--- src/components/Asteroid.jsx
+++ src/components/Asteroid.jsx
@@ -24,36 +24,47 @@ const Asteroid = () => {
     getAsteroid();
   }, []);
 
-  const asteroids = neo["near_earth_objects"] && neo["near_earth_objects"][today];
+  const asteroids =
+    neo["near_earth_objects"] && neo["near_earth_objects"][today];
 
-  // Map each asteroid data point to the table to display the name, size, hazard status, and approach time to the user. 
-  const asteroidTable = asteroids && asteroids.map((asteroid, index) => {
-    return (
-      <tr key={index}>
-        <td>{asteroid.name}</td>
-        <td>
-          {asteroid.estimated_diameter.miles.estimated_diameter_max.toFixed(3)} miles
-        </td>
-        <td>{asteroid.is_potentially_hazardous_asteroid ? "☢️" : "❎"}</td>
-        <td>{asteroid.close_approach_data[0].close_approach_date_full}</td>
-      </tr>
-    );
-  });
+  // Map each asteroid data point to the table to display the name, size, hazard status, and approach time to the user.
+  const asteroidTable =
+    asteroids &&
+    asteroids.map((asteroid, index) => {
+      return (
+        <tr key={index}>
+          <td>{asteroid.name}</td>
+          <td>
+            {asteroid.estimated_diameter.miles.estimated_diameter_max.toFixed(
+              3
+            )}{" "}
+            miles
+          </td>
+          <td>{asteroid.is_potentially_hazardous_asteroid ? "☢️" : "❎"}</td>
+          <td>{asteroid.close_approach_data[0].close_approach_date_full}</td>
+        </tr>
+      );
+    });
 
   return (
-    <div className="overflow-x-auto" data-theme="night">
-      <table className="table table-zebra w-full text-center items-center justify-center">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>Asteroid Name</th>
-            <th>Size {"(max diameter)"}</th>
-            <th>Potentially Hazardous?</th>
-            <th>Approach Date</th>
-          </tr>
-        </thead>
-        <tbody>{asteroidTable}</tbody>
-      </table>
+    <div className="card bg-base-100 text-neutral-content">
+      <h1 className="text-center font-semibold antialiased text-3xl mt-4">
+        Asteroid Watch
+      </h1>
+      <div className="card-body items-center text-center">
+        <table className="table table-zebra w-full text-center items-center justify-center">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Asteroid Name</th>
+              <th>Size {"(max diameter)"}</th>
+              <th>Potentially Hazardous?</th>
+              <th>Approach Date</th>
+            </tr>
+          </thead>
+          <tbody>{asteroidTable}</tbody>
+        </table>
+      </div>
     </div>
   );
 };
