be1ca43ffed51079bedb8e4005002f60ff554ebc
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-06-03 13:54:32 -0500

    globe nyc

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Viewer } from 'resium'
+import React from "react";
+import { Entity, EntityDescription, Viewer, CameraFlyTo } from "resium";
+import { Cartesian3 } from "cesium";
 
 const Globe = () => {
-  return (
-    <div><Viewer /></div>
-  )
-}
+  const position = Cartesian3.fromDegrees(-74.0707, 40.711, 1000);
+  const pointGraphics = { pixelSize: 10 };
+  const globeStyle = {
+    position: "absolute",
+    top: 72,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
 
-export default Globe
+  return (
+    <Viewer style={globeStyle}>
+      <CameraFlyTo duration={5} destination={position}>
+        <Entity position={position} point={pointGraphics}>
+          <EntityDescription>
+            <h1 className="text-black">Hello World!</h1>
+          </EntityDescription>
+        </Entity>
+      </CameraFlyTo>
+    </Viewer>
+  );
+};
+
+export default Globe;
