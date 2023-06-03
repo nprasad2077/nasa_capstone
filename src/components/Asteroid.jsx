f7d488282ac763b9df01ecf38594617070300bc9
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-06-03 17:40:33 -0500

    all padded up!

--- src/components/Asteroid.jsx
+++ src/components/Asteroid.jsx
@@ -48,7 +48,7 @@ const Asteroid = () => {
     });
 
   return (
-    <div className="card bg-base-100 text-neutral-content mx-auto">
+    <div className="card bg-base-100 text-neutral-content mx-auto w-full">
       <h1 className="text-center font-semibold antialiased text-3xl mt-4">
         Asteroid Watch
       </h1>
