62e7c3707dcb56b4b48d6f1438d8868283826021
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-06-05 14:09:49 -0500

    responsive components!

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import '../styles/Earth.css'
+import "../styles/Earth.css";
 
 const Earth = () => {
-    // Get yesterday's date in a format suitable for the API call.
+  // Get yesterday's date in a format suitable for the API call.
   let epicDate = ((d) => new Date(d.setDate(d.getDate() - 2)))(new Date())
     .toISOString()
     .slice(0, 10);
@@ -14,7 +14,7 @@ const Earth = () => {
   const [earth, setEarth] = useState([]);
   const [images, setImages] = useState([]);
 
-// Fetch earth images by date.
+  // Fetch earth images by date.
   const getEarth = async () => {
     try {
       const response = await axios.get(
@@ -41,31 +41,29 @@ const Earth = () => {
 
   return (
     <div className="card bg-base-100 text-neutral-content flex flex-col items-center">
-      <div className="items-center justify-center">
-        <div className="card bg-base-100 text-neutral-content flex flex-col items-center">
-          <h2 className="text-center font-bold antialiased text-3xl mt-4">
-            Earth Polychromatic Imaging Camera {"(EPIC)"}
-          </h2>
-          <div className="px-10 pt-10">
-          <div className="carousel-container">
-              {images ? <ImageGallery
-                items={images}
-                autoPlay={false}
-                showPlayButton={false}
-                showFullscreenButton={true}
-              /> : <progress data-theme='night' className="progress w-72"></progress> }
-              
-            </div>
-          </div>
-          <div className="card-body items-center text-center text-sm mb-6">
-            <p>
-              These images were taken by NASA's EPIC camera onboard the NOAA DSCOVR
-              spacecraft
-            </p>
-            <p>The most recent images are from: {date}</p>
-            
-          </div>
-        </div>
+      <h2 className="text-center font-bold antialiased text-3xl mt-4">
+        Earth Polychromatic Imaging Camera {"(EPIC)"}
+      </h2>
+
+      <div className="carousel-container">
+        {images ? (
+          <ImageGallery
+            items={images}
+            autoPlay={false}
+            showPlayButton={false}
+            showFullscreenButton={true}
+          />
+        ) : (
+          <progress data-theme="night" className="progress w-72"></progress>
+        )}
+      </div>
+
+      <div className="card-body items-center text-center text-sm mb-6">
+        <p>
+          These images were taken by NASA's EPIC camera onboard the NOAA DSCOVR
+          spacecraft
+        </p>
+        <p>The most recent images are from: {date}</p>
       </div>
     </div>
   );
