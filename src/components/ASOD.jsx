0e5a8ae07d23c331bdd3eb71d469104baf270e94
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-06-04 19:26:51 -0500

    save button functionality

--- src/components/ASOD.jsx
+++ src/components/ASOD.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../styles/ASOD.css";
 
 const ASOD = () => {
   const [asod, setAsod] = useState({});
   const [media, setMedia] = useState(null);
+  // State to hold favorites image data
+  const [favorites, setFavorites] = useState([]);
+  // Loading state for images component. Will load once data has arrived.
+  const [loading, setLoading] = useState(true);
 
   // Fetch ASOD media
   const getAsod = async () => {
@@ -47,14 +52,65 @@ const ASOD = () => {
     }
   }, [asod]);
 
+  const getFavorites = async () => {
+    try {
+      const response = await axios.get(
+        `https://calm-brushlands-38440.herokuapp.com/`
+      );
+      setFavorites(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  const images =
+    favorites.length > 0
+      ? favorites
+          .filter((img) => img.name === "ASOD")[0]
+          .postPhoto.map((img, i) => img)
+      : [];
+
+  console.log([...images, asod.hdurl]);
+
+  const saveButton = () => {
+    try {
+      const response = axios.put(
+        `https://calm-brushlands-38440.herokuapp.com/update`,
+        { _id: import.meta.env.VITE_APP_HERO_KEY, postPhoto: [...images, asod.hdurl] }
+      );
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="card bg-base-100 text-neutral-content">
-      <h1 className="text-center font-semibold antialiased text-3xl mt-4">
+      <h1 className="text-center font-bold antialiased text-3xl mt-4">
         {asod.title}
       </h1>
       <figure className="px-10 pt-10">{media}</figure>
       <div className="card-body items-center text-center">
         <p>{asod.explanation}</p>
+        <p className="my-4">
+          NASA Astronomy Picture of the Day: Each day, a different image of our
+          fascinating universe is featured, along with a brief statement written
+          by a professional astronomer.
+        </p>
+      </div>
+      <div className="self-center p-4">
+        <button
+          onClick={saveButton}
+          className="btn btn-neuttral myButton"
+          data-theme="night"
+        >
+          ❤️
+        </button>
       </div>
     </div>
   );
