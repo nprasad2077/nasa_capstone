4ed5731be9296da635694ebda0407549ffc0fee6
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-08-08 19:21:28 -0500

    globe overlay working

--- src/components/NewGlobe.jsx
+++ src/components/NewGlobe.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Globe from 'react-globe.gl'
+import React from 'react';
+import Globe from 'react-globe.gl';
 
 const NewGlobe = () => {
+  const globeEl = React.useRef(null);
+
+  React.useEffect(() => {
+    // Initialize the globe
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.5;
+  }, []);
+
   return (
-    <div>
-        <Globe />
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Globe
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology-min.jpg"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default NewGlobe
-
+export default NewGlobe;
