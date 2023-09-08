30ccf73aabee2be74243a366bc5bb9410b1f811a
Nikhil Prasad <nprasad2077@gmail.com>
2023-09-08 01:10:43 -0500

    media player changes

--- src/components/MediaPlayer.jsx
+++ src/components/MediaPlayer.jsx
@@ -22,12 +22,6 @@ const MediaPlayer = () => {
         const mp4File = result.data.find((item) => item.endsWith("orig.mp4"));
         if (mp4File) {
           setSelectedMediaUrl(mp4File);
-          // Manually reload the video player if it exists
-          const videoElement = document.getElementById("videoPlayer");
-          if (videoElement) {
-            videoElement.load();
-            videoElement.play();
-          }
         } else {
           setError("No MP4 file found in the collection");
         }
@@ -78,19 +72,19 @@ const MediaPlayer = () => {
           value={keyword}
           onChange={handleInputChange}
           placeholder="Search keyword"
-          className="input input-bordered input-accent flex-grow"
+          className="border border-gray-300 focus:outline-none px-4 py-2 rounded flex-grow"
         />
-        <button onClick={handleSearch} className="btn btn-accent flex-shrink">
+        <button onClick={handleSearch} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
           Search
         </button>
       </div>
       {isLoading && (
         <div className="text-center">
-          <span className="loading loading-spinner loading-lg"></span>
+          <span className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-gray-900"></span>
         </div>
       )}
       {error && <div className="text-center text-red-500">{error}</div>}
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 gap-4 m-10">
         {mediaList.map((item, index) => (
           <div
             key={index}
@@ -100,18 +94,16 @@ const MediaPlayer = () => {
             <img
               src={item.links[0].href}
               alt="preview"
-              className="w-full object-cover"
+              className="w-full object-cover rounded"
             />
           </div>
         ))}
       </div>
       {selectedMediaUrl && (
-        <div className="mt-8">
-          <video id="videoPlayer" controls width="100%">
+          <video controls autoPlay className="max-w-full max-h-full mt-6">
             <source src={selectedMediaUrl} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-        </div>
       )}
     </div>
   );
